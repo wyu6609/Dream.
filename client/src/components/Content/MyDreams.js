f0db73a4987023198dc98510913a6ea84f62997e
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-06-02 18:01:28 -0400

    added pagination my dreams

--- client/src/components/Content/MyDreams.js
+++ client/src/components/Content/MyDreams.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from "react";
 import Post from "./Post";
 import axios from "axios";
+import AppPagination from "../pagination";
+let pageSize = 5;
 const MyDreams = ({
   user,
   dreamwall,
-  setDreamWall,
+
   handleDelete,
   setDreamDescription,
   handleOpen,
 }) => {
+  const [pagination, setPagination] = React.useState({
+    count: 0,
+    from: 0,
+    to: pageSize,
+  });
   const dreams = dreamwall
     .filter((el) => el.user.id === user.id)
+    .slice(pagination.from, pagination.to)
     .map((dream) => {
       return (
         <Post
@@ -22,8 +30,24 @@ const MyDreams = ({
         />
       );
     });
+  useEffect(() => {
+    setPagination({ ...pagination, count: dreams.length });
+  }, []);
+  const handlePageChange = (event, page) => {
+    const from = (page - 1) * pageSize;
+    const to = (page - 1) * pageSize + pageSize;
+    setPagination({ ...pagination, from: from, to: to });
+  };
 
-  return <div className="post-container">{dreams}</div>;
+  return (
+    <>
+      <div className="post-container">{dreams}</div>
+      <AppPagination
+        pagination={pagination}
+        handlePageChange={handlePageChange}
+      />
+    </>
+  );
 };
 
 export default MyDreams;
